394c809a85477b3851342ed65ae042fdbbd161cb
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-21 15:21:12 +0530

    Ex 1.27 - Reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0
+with open('Data/portfolio.csv', 'rt') as stocks_file:
+    headers = next(stocks_file).split(',')
+    # For each stock calculate the total cost
+    for each_company_line in stocks_file:
+        each_company_list = each_company_line.split(',')
+        total_cost += int(each_company_list[1]) * float(each_company_list[2].strip())
+print(f"Total cost {total_cost}")
