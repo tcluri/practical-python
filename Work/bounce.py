117a25964ecfb808d6c5ebc5ae56e5060aa899c2
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-21 11:48:58 +0530

    Add bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+init_height = 100
+bounce_height = 3/5
+bounces = 10
+rounding_decimals = 4
+for i in range(bounces):
+    next_height = init_height * bounce_height
+    print(i+1, round(next_height, rounding_decimals))
+    init_height = next_height
