5918fb2ff51b22a1f16cc334a31adcd38fd7287f
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-21 15:41:46 +0530

    Ex 1.30 Error handling for Data/missing.csv file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,21 @@
 # pcost.py
 #
-# Exercise 1.27
-total_cost = 0
-with open('Data/portfolio.csv', 'rt') as stocks_file:
-    headers = next(stocks_file).split(',')
-    # For each stock calculate the total cost
-    for each_company_line in stocks_file:
-        each_company_list = each_company_line.split(',')
-        total_cost += int(each_company_list[1]) * float(each_company_list[2].strip())
-print(f"Total cost {total_cost}")
+# Exercise 1.31
+
+def portfolio_cost(filename):
+    total_cost = 0
+    with open(filename, 'rt') as stocks_file:
+        headers = next(stocks_file).split(',')
+        # For each stock calculate the total cost
+        for each_company_line in stocks_file:
+            each_company_list = each_company_line.split(',')
+            try:
+                num_shares = int(each_company_list[1])
+            except ValueError:
+                print(f"Warning! There are no shares for {each_company_list[0]}")
+                continue
+            total_cost += num_shares * float(each_company_list[2].strip())
+    return total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f"Total cost: {cost}")
