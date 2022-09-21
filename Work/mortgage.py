fca778b3d89fd1ea109d4c74d44336c68e9cb3ac
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-21 12:20:53 +0530

    Ex 1.10 Making a table

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,18 +8,16 @@ total_paid = 0.0
 extra_payment_start_month = 61
 extra_payment_end_month = 108
 extra_payment = 1e3
-months_num = 0
+month_num = 0
 
 while principal > 0:
-    months_num += 1
+    month_num += 1
     actual_payment = payment
-    if extra_payment_start_month <= months_num <= extra_payment_end_month:
+    if extra_payment_start_month <= month_num <= extra_payment_end_month:
         actual_payment = payment + extra_payment
     principal = (principal * (1+rate/12)) - (actual_payment)
     total_paid = total_paid + actual_payment
-    # print("Months", months_num)
-    # print("Actual payment", actual_payment)
+    print(month_num, round(total_paid, 2), round(principal,2))
 
-
-print('Total paid', round(total_paid, 2))
-print('Months payed', round(months_num, 2))
+print("Total paid", round(total_paid, 2))
+print("Months", month_num)
