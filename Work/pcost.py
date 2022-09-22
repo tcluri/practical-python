5c2929da2228bc41d67264107e6be3ab4154b680
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-22 09:49:22 +0530

    Ex 2.15 - A practical enumerate() example

--- Work/pcost.py
+++ Work/pcost.py
@@ -8,13 +8,14 @@ def portfolio_cost(filename):
     total_cost = 0
     with open(filename, 'rt') as stocks_file:
         rows = csv.reader(stocks_file)
-        headers = next(rows)
+        # headers = next(rows)
         # For each stock calculate the total cost
-        for each_company_row in rows:
+        for row_num, each_company_row in enumerate(rows, start=1):
             try:
                 num_shares = int(each_company_row[1])
             except ValueError:
-                print(f"Warning! There are no shares for {each_company_row[0]}")
+                print(f'Row {row_num}: Bad row: {each_company_row}')
+                # print(f"Warning! There are no shares for {each_company_row[0]}")
                 continue
             total_cost += num_shares * float(each_company_row[2])
     return total_cost
