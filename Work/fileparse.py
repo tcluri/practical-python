0b978ca9342fcfe8d022e5c905a8c4a5b71d124b
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-24 15:16:57 +0530

    Ex 3.4 - Building a column selector

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,19 +3,28 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=[]):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f)
 
+
         # Read the file headers
         headers = next(rows)
+        # Select only desired columns
+        if select:
+            indices = [headers.index(select_ind) for select_ind in select]
+            headers = select
+        else:
+            indices = []
         records = []
         for row in rows:
             if not row:    # Skip rows with no data
                 continue
+            if indices:
+                row = [row[index] for index in indices]
             record = dict(zip(headers, row))
             records.append(record)
 
