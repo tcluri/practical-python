806c8b4e4254cb22f8c497c11da11b85ad5cb133
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-24 16:03:54 +0530

    Ex 3.8 - Raising exceptions

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -7,6 +7,8 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
     '''
     Parse a CSV file into a list of records
     '''
+    if select and not has_headers:
+        raise RuntimeError("select argument requires column headers")
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
         # Indices of the select columns
