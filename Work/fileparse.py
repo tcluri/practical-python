e98d480c3c8fbcbf8df7680d7e21ec9598bb01dd
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-24 15:36:40 +0530

    Ex 3.6 - Working without Headers

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,22 +3,23 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=False):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f)
-
-
-        # Read the file headers
-        headers = next(rows)
-        # Select only desired columns
-        if select:
-            indices = [headers.index(select_ind) for select_ind in select]
-            headers = select
-        else:
-            indices = []
+        # Indices of the select columns
+        indices = []
+        # Check if the headers are present
+        if has_headers:
+            # Read the file headers
+            headers = next(rows)
+            # Select only desired columns
+            if select:
+                indices = [headers.index(select_ind) for select_ind in select]
+                headers = select
+        # Build the output for records
         records = []
         for row in rows:
             if not row:    # Skip rows with no data
@@ -27,7 +28,10 @@ def parse_csv(filename, select=None, types=None):
                 row = [row[index] for index in indices]
             if types:
                 row = [func(val) for func, val in zip(types, row)]
-            record = dict(zip(headers, row))
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
