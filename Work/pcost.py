e0e02c250c40ddeca5613924a7dbad8b794c5c20
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-26 15:33:46 +0530

    Ex 3.14 - Using more library imports

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,23 +3,25 @@
 # Exercise 1.33 Reading from command line
 import sys
 import csv
+from report import read_portfolio
 
 def portfolio_cost(filename):
-    total_cost = 0
-    with open(filename, 'rt') as stocks_file:
-        rows = csv.reader(stocks_file)
-        headers = next(rows)
-        # For each stock calculate the total cost
-        for row_num, each_company_row in enumerate(rows, start=1):
-            record = dict(zip(headers, each_company_row))
-            try:
-                num_shares = int(record['shares'])
-                price = float(record['price'])
-                total_cost += num_shares * price
-            except ValueError:
-                print(f'Row {row_num}: Bad row: {each_company_row}')
+    # total_cost = 0
+    # with open(filename, 'rt') as stocks_file:
+    #     rows = csv.reader(stocks_file)
+    #     headers = next(rows)
+    #     # For each stock calculate the total cost
+    #     for row_num, each_company_row in enumerate(rows, start=1):
+    #         record = dict(zip(headers, each_company_row))
+    #         try:
+    #             num_shares = int(record['shares'])
+    #             price = float(record['price'])
+    #             total_cost += num_shares * price
+    #         except ValueError:
+    #             print(f'Row {row_num}: Bad row: {each_company_row}')
                 # print(f"Warning! There are no shares for {each_company_row[0]}")
-    return total_cost
+    return sum([each_company['shares'] * each_company['price']
+                for each_company in read_portfolio(filename)])
 
 
 if len(sys.argv) == 2:
