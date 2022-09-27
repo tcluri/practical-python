64d0df928deb79e91d4191696a0cab4f3f915418
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-27 11:42:58 +0530

    Ex 4.2 - Adding some methods

---
+++ Work/stock.py
@@ -0,0 +1,9 @@
+class Stock:
+    def __init__(self, stockname, shares, price):
+        self.name = stockname
+        self.shares = shares
+        self.price = price
+    def cost(self):
+        return self.shares * self.price
+    def sell(self, num_shares):
+        self.shares -= num_shares
