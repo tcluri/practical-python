f4adbe5ccd3d25b46d7b62ce117edd475209e0b8
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-28 18:37:17 +0530

    Ex 6.10 - Making more pipeline components

--- Work/follow.py
+++ Work/follow.py
@@ -15,10 +15,14 @@ def follow(stocksfile):
 
 
 if __name__ == "__main__":
- for line in follow('Data/stocklog.csv'):
-    fields = line.split(',')
-    name = fields[0].strip('"')
-    price = float(fields[1])
-    change = float(fields[4])
-    if change < 0:
-        print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+    import report
+
+    portfolio = report.read_portfolio('Data/portfolio.csv')
+
+    for line in follow('Data/stocklog.csv'):
+       fields = line.split(',')
+       name = fields[0].strip('"')
+       price = float(fields[1])
+       change = float(fields[4])
+       if name in portfolio:
+           print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
