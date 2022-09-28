2cc1aadb9f025643056ad1005790e9e0353278d5
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-28 18:17:26 +0530

    Ex 6.6 - Using a generator to produce data

--- Work/follow.py
+++ Work/follow.py
@@ -2,14 +2,20 @@
 import os
 import time
 
-f = open('Data/stocklog.csv')
-f.seek(0, os.SEEK_END)   # Move file pointer 0 bytes from end of file
+def follow(stocksfile):
+    f = open(stocksfile)
+    f.seek(0, os.SEEK_END)   # Move file pointer 0 bytes from end of file
 
-while True:
-    line = f.readline()
-    if line == '':
-        time.sleep(0.1)   # Sleep briefly and retry
-        continue
+    while True:
+        line = f.readline()
+        if line == '':
+            time.sleep(0.1)   # Sleep briefly and retry
+            continue
+        yield line
+
+
+if __name__ == "__main__":
+ for line in follow('Data/stocklog.csv'):
     fields = line.split(',')
     name = fields[0].strip('"')
     price = float(fields[1])
