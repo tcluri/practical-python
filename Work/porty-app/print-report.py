c9e54853811b776aeafefbee7e9ce18911502e6f
Tejaa Chintaluri <saitejaac@gmail.com>
2022-09-29 19:34:03 +0530

    Ex 9.5 - Make a package

--- Work/porty-app/print-report.py
+++ Work/porty-app/print-report.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-# print-report.py
 import sys
+
+# Above works and being used
+
+# print-report.py - Activate practical-python/.venv and
+# change directory and run from porty-app/ directory
 from porty.report import main
 main(sys.argv)
---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Tejaa Chintaluri",
+    author_email="tcluri@example.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
